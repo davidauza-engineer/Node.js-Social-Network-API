bc091a3c946a5add8322e9e780b950ac4d88c735
davidauza.engineer <davidauza01@gmail.com>
2020-08-27 21:50:14 -0500

    Add routes to default route "/"

--- app.js
+++ app.js
@@ -22,11 +22,24 @@ const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const expressValidator = require("express-validator");
+const fs = require("fs");
 
 // bring in routes
 const postRoutes = require("./routes/post");
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
+// apiDocs
+app.get("/", (req, res) => {
+  fs.readFile("docs/apiDocs.json", (err, data) => {
+    if (err) {
+      res.status(400).json({
+        error: err
+      });
+    }
+    const docs = JSON.parse(data);
+    res.json(docs);
+  });
+});
 
 // middleware
 app.use(morgan("dev"));
